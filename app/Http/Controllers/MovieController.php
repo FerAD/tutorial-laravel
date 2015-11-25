82f53fe7c04e48895bac39a902e84b232eaf1f4a
Fernando Arey Durán <farey.duran@gmail.com>
2015-11-25 13:23:03 -0600

    Controller REST y Modelos
    Se agregó el controller MovieController con rutas RESTful y dos
    modelos: movie y genre

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Cinema\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Cinema\Http\Requests;
+use Cinema\Http\Controllers\Controller;
+
+class MovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return "estoy en el indes";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return "Esto seria el formulario para crear";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return "aqui se realizaria el store";
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return "aqiu se realiza el show";
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return "Aqui se realiza el edit";
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return "Aqui se reazlia el update";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return "aqui se realiza el destroy";
+    }
+}
